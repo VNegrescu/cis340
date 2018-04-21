ab7710fec79e558974af9369d519b09b68616f8f
VNegrescu <vnegrescu@gmail.com>
2018-04-21 12:11:25 -0400

    Add files via upload

---
+++ toplevel.c
@@ -0,0 +1,34 @@
+#include	<stdio.h>
+#include	<stdlib.h>
+
+struct node{
+	int val;
+	struct node * next;
+};
+
+int main()
+{
+	struct node *head = NULL;	/* declare head & tail */
+	struct node *tail = NULL;
+	struct node *current = NULL;
+
+	head = malloc(sizeof(struct node));	/* create first node & append to list */
+	head->val = 1;
+	tail = head;
+
+	tail->next = malloc(sizeof(struct node));	/*link 1st node with new node */
+	tail = tail->next;				/* update position of tail */
+	tail->val = 22;
+
+	tail->next = malloc(sizeof(struct node));
+	tail = tail->next;
+	tail->val = 3;
+	tail->next = NULL;				/* end of list	*/
+
+	current = head;
+	while( current != NULL)
+	{
+		printf("%i\n", current->val);
+		current = current->next;
+	}
+}
